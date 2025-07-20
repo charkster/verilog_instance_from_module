d175e041a19a36ad6c5002073696621942df90af
charkster <31560568+charkster@users.noreply.github.com>
2025-07-20 15:31:14 -0700

    Update verilog_instance_from_module.py

--- verilog_instance_from_module.py
+++ verilog_instance_from_module.py
@@ -2,7 +2,7 @@ import re
 with open('C:\\verilog_file.sv', 'r') as file:
     file_content = file.read()
 # port[0] is input|output|inout, port[1] is reg|wire|logic, port[2] is width, port[3] is name
-port_pattern = r"\n\s*(input|output|inout)\s+(reg|wire|logic)?\s*(\[\d+:\d+\])?\s+(\w+)\s*"
+port_pattern = r"\n\s*(input|output|inout)\s+(reg|wire|logic)?\s*(\[.*\])?\s+(\w+)\s*"
 ports = re.findall(port_pattern, file_content)
 len_longest_name = 0
 for port in ports:
